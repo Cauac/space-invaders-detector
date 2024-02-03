1641679611b325bbffb2c7749f88ce0157e7f06b
Anton Garansky <anton.garansky@telenordigital.com>
2024-02-03 13:51:33 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject space-invaders-detector "0.1.0-SNAPSHOT"
+  :description "Space invaders detector"
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns space-invaders-detector.core})

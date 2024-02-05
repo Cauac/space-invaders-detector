cbbd728a08a10767c2db6229b8c9c4478428aacc
Anton Garansky <anton.garansky@telenordigital.com>
2024-02-05 20:06:40 +0100

    implement matrix util namespace

---
+++ src/space_invaders_detector/matrix.clj
@@ -0,0 +1,68 @@
+(ns space-invaders-detector.matrix
+  (:require [clojure.string :as str]))
+
+(defn str->matrix
+  "Converts text representation of matrix into 2-dimensional array of symbols.
+  Example:
+   '-o-     [[- o -]
+    ooo  =>  [o o o]
+    -o-'     [- o -]]"
+  [^CharSequence s]
+  (when-not (empty? s)
+    (->> (str/split-lines s)
+         (mapv vec))))
+
+(defn height [matrix]
+  (count matrix))
+
+(defn width [matrix]
+  (count (first matrix)))
+
+(defn size [matrix]
+  (* (height matrix) (width matrix)))
+
+(defn find-possible-locations
+  "For given matrix finds all possible locations where another matrix can fit.
+   Returns list of locations, each element is [Y1, X1, Y2, X2] coordinates
+   Examples:
+    By providing another matrix size parameters
+     [[ 1 2 ]  , h=1, w=2 => [[0 0 0 1] [1 0 1 1]]
+      [ 3 4 ]]
+    By providing another matrix
+     [[ 1 2 ]  , [ * * ] => [[0 0 0 1] [1 0 1 1]]
+      [ 3 4 ]]
+   "
+  ([matrix another-matrix]
+   (find-possible-locations matrix (height another-matrix) (width another-matrix)))
+  ([matrix m-height m-width]
+   (when (and (pos? m-height) (pos? m-width))
+     (let [matrix-height (height matrix)
+           matrix-width (width matrix)]
+       (for [y1 (range (- matrix-height m-height -1))
+             x1 (range (- matrix-width m-width -1))
+             :let [y2 (+ y1 m-height -1)
+                   x2 (+ x1 m-width -1)]]
+         [y1 x1 y2 x2])))))
+
+(defn crop
+  "Returns sub-matrix defined by its top-left and bottom-right coordinates."
+  [matrix [y1 x1 y2 x2]]
+  (->> (subvec matrix y1 (inc y2))
+       (mapv #(subvec % x1 (inc x2)))))
+
+(defn- match-elements [e1 e2]
+  (if (= e1 e2) 1 0))
+
+(defn- match-vectors [v1 v2]
+  (reduce + (map match-elements v1 v2)))
+
+(defn rate-match
+  "Calculates match of two matrices in percentages"
+  [matrix another-matrix]
+  (let [equal-elements (reduce + (map match-vectors matrix another-matrix))
+        size (size matrix)]
+    (/ (* 100.0 equal-elements) size)))
+
+(defn print-matrix [matrix]
+  (doseq [vector matrix]
+    (println (apply str vector))))
---
+++ test/space_invaders_detector/matrix_test.clj
@@ -0,0 +1,128 @@
+(ns space-invaders-detector.matrix-test
+  (:require [clojure.test :refer :all]
+            [space-invaders-detector.matrix :refer :all]))
+
+(def matrix-1x1 [[0]])
+(def matrix-2x2 [[0 0]
+                 [1 1]])
+(def matrix-4x3 [[0 0 0]
+                 [1 1 1]
+                 [2 2 2]
+                 [3 3 3]])
+(def matrix-4x1 [[0]
+                 [1]
+                 [2]
+                 [3]])
+
+(deftest str->matrix-test
+  (testing "Basic test"
+    (let [input "123\n456"
+          expected [[\1 \2 \3]
+                    [\4 \5 \6]]]
+      (is (= expected (str->matrix input)))))
+  (testing "Empty string input"
+    (is (= nil (str->matrix ""))))
+  (testing "Blank string input"
+    (is (= [[\space \space]] (str->matrix "  "))))
+  (testing "String with special characters"
+    (let [input " #\t\n ^$"
+          expected [[\space \# \tab]
+                    [\space \^ \$]]]
+      (is (= expected (str->matrix input))))))
+
+(deftest matrix-height-test
+  (testing "Basic test"
+    (is (= 1 (height matrix-1x1)))
+    (is (= 2 (height matrix-2x2)))
+    (is (= 4 (height matrix-4x3)))
+    (is (= 4 (height matrix-4x1))))
+  (testing "Empty matrix"
+    (is (= 0 (height nil)))))
+
+(deftest matrix-width-test
+  (testing "Basic tests"
+    (is (= 1 (width matrix-1x1)))
+    (is (= 2 (width matrix-2x2)))
+    (is (= 3 (width matrix-4x3)))
+    (is (= 1 (width matrix-4x1))))
+  (testing "Empty matrix"
+    (is (= 0 (width nil)))))
+
+(deftest matrix-size-test
+  (testing "Basic test"
+    (is (= 1 (size matrix-1x1)))
+    (is (= 4 (size matrix-2x2)))
+    (is (= 12 (size matrix-4x3)))
+    (is (= 4 (size matrix-4x1))))
+  (testing "Empty matrix"
+    (is (= 0 (size nil)))))
+
+(deftest find-possible-locations-test
+  (testing "Basic tests with height and width"
+    (is (= [[0 0 3 2]] (find-possible-locations matrix-4x3 4 3)))
+    (is (= [[0 0 3 1] [0 1 3 2]] (find-possible-locations matrix-4x3 4 2)))
+    (is (= [[0 0 3 0] [0 1 3 1] [0 2 3 2]] (find-possible-locations matrix-4x3 4 1)))
+    (is (= [[0 0 2 1] [0 1 2 2] [1 0 3 1] [1 1 3 2]] (find-possible-locations matrix-4x3 3 2)))
+    (is (= [[0 0 1 1] [0 1 1 2] [1 0 2 1] [1 1 2 2] [2 0 3 1] [2 1 3 2]] (find-possible-locations matrix-4x3 2 2))))
+  (testing "Basic tests with another matrix"
+    (is (= [[0 0 3 2]] (find-possible-locations matrix-4x3 matrix-4x3)))
+    (is (= [[0 0 3 0] [0 1 3 1] [0 2 3 2]] (find-possible-locations matrix-4x3 matrix-4x1)))
+    (is (= [[0 0 1 1] [0 1 1 2] [1 0 2 1] [1 1 2 2] [2 0 3 1] [2 1 3 2]] (find-possible-locations matrix-4x3 matrix-2x2)))
+    (is (= [[0 0 0 0] [0 1 0 1] [1 0 1 0] [1 1 1 1]] (find-possible-locations matrix-2x2 matrix-1x1))))
+  (testing "Testing against bigger matrix. Expected no possible locations."
+    (is (= [] (find-possible-locations matrix-1x1 matrix-2x2)))
+    (is (= [] (find-possible-locations matrix-1x1 matrix-4x3)))
+    (is (= [] (find-possible-locations matrix-4x1 matrix-4x3))))
+  (testing "One or both matrices are empty"
+    (is (= [] (find-possible-locations [] 2 2)))
+    (is (= nil (find-possible-locations [] 0 0)))
+    (is (= nil (find-possible-locations [] [])))
+    (is (= nil (find-possible-locations [] nil)))
+    (is (= nil (find-possible-locations nil nil)))
+    (is (= nil (find-possible-locations matrix-4x3 0 0)))))
+
+(deftest crop-test
+  (testing "Basic tests"
+    (is (= [[0]] (crop matrix-2x2 [0 0 0 0])))
+    (is (= [[1 1]] (crop matrix-2x2 [1 0 1 1])))
+    (is (= matrix-4x1 (crop matrix-4x3 [0 0 3 0])))
+    (is (= [[1 1] [2 2]] (crop matrix-4x3 [1 1 2 2])))))
+
+(deftest crop-integration-test
+  (testing "Integration possible location search and crop function"
+    (testing "For 2x2 matrix and 2x1 search pattern it should return two cut options"
+      (let [matrix [["11" "12"]
+                    ["21" "22"]]
+            search-matrix [["*"]
+                           ["*"]]
+            expected-cut-1 [["11"]
+                            ["21"]]
+            expected-cut-2 [["12"]
+                            ["22"]]
+            results (->> (find-possible-locations matrix search-matrix)
+                         (map #(crop matrix %)))]
+        (is (= [expected-cut-1 expected-cut-2] results))))))
+
+(deftest rate-match-test
+  (testing "Checking match of matrices. The more equal element the bigger the score."
+    (let [m-1 [[\# \#] [\@ \@]]
+          m-2 [[\* \*] [\* \*]]]
+      (is (= 0.0 (rate-match m-1 m-2))))
+    (let [m-1 [[\# \#] [\@ \@]]
+          m-2 [[\@ \@] [\@ \@]]]
+     (is (= 50.0 (rate-match m-1 m-2))))
+    (let [m-1 [[\# \#] [\@ \@]]
+          m-2 [[\@ \#] [\@ \@]]]
+      (is (= 75.0 (rate-match m-1 m-2))))
+    (let [m-1 [[\# \#] [\@ \@]]
+          m-2 [[\# \#] [\@ \@]]]
+      (is (= 100.0 (rate-match m-1 m-2))))
+    (let [m-1 [[\# \#] [\@ \@]]
+          m-2 [[\# \#] [\@ \@]]]
+      (is (= 100.0 (rate-match m-1 m-2))))
+
+    (testing "Unequal size matrices compared by the smallest one."
+     (is (= 100.0 (rate-match matrix-4x1 matrix-4x3)))
+     (let [m-1 [[\# \*]]
+           m-2 [[\# \#] [\@ \@]]]
+       (is (= 50.0 (rate-match m-1 m-2)))))))
